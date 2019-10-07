b2ac8b357ac3e7a8eb36140116c0ce4faaca8090
Lehi <lqmyacs@gmail.com>
2019-10-07 01:03:59 -0500

    qr code card for studenst

--- resources/views/events/downloadQr.blade.php
+++ resources/views/events/downloadQr.blade.php
@@ -3,26 +3,12 @@
 <html lang="en">
 <head>
   <title>Bootstrap Example</title>
-  <meta charset="utf-8style="width:105mm;height:148mm"
-">
+  
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<style>
-html { margin: 0px}
-body {
-  
-    background: url(https://i.imgur.com/OUYkA33.png);    
-}
-div{
-    margin-top : 10px;
-    margin-left : 10px;
-    margin-right : 10px;
-
-}
-</style>
 </head>
 <body> 
 
@@ -34,11 +20,11 @@ div{
 <h4><strong>Escuela Profesinoal de Ciencia de la Computación</strong></h4>
 <hr>
 
-  <div class="col-xs-3">
+  <div class="col-xs-4">
     <img style="width:100%;" src="data:image/png;base64, {{ base64_encode(QrCode::encoding('UTF-8')->format('png')->margin(1)->size(220)->generate( $cui )) }}">
   </div>
 
-  <div class="col-xs-9">
+  <div class="col-xs-8">
     <h4><strong>Apellidos:</strong> {{$apellidos}} </h4>
     <h4><strong>Nombres:</strong> {{$nombres}} </h4>
     <h4><strong>CUI:</strong> {{$cui}}</h4>
