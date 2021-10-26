33bbe4d424366e5762a8c8ab604fffae85efab5b
alrus2797 <alrus2797@gmail.com>
2021-10-25 20:53:23 -0500

    Create 2021 page and set url to 2020 event

---
+++ resources/views/index21.blade.php
@@ -0,0 +1,17 @@
+@extends('templates.themeVue')
+
+@section('title', 'School Of Computer Science UNSA')
+
+@section('scripts')
+
+@endsection
+
+@section('home', 'active')
+
+@section('body')
+
+	<header-component/>
+	{{-- <ponents-component/> --}}
+	
+
+@endsection
