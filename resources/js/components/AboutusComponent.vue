6b7e68d2fb848ee46fe9ab87e61c3772e981f062
faoladhaku <angelsoulsword@gmail.com>
2021-10-29 13:37:53 -0500

    aboutus component to review

---
+++ resources/js/components/AboutusComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <section id="about-section" class="about-section section theme-bg-light">
+            <div class="container">
+                <h3 class="section-heading text-center mb-3">Sobre Nosotros</h3>
+                <div class="section-intro single-col-max mx-auto mb-4">La <strong>Escuela Profesional de Ciencia de la Computación</strong> tiene por objetivo
+                    formar profesionales aptos para <strong>identificar problemas del mundo real y proponer soluciones inéditas o mejorar las ya existentes</strong>,
+                    convirtiéndolos en líderes de la realización de un determinado proyecto por intermedio de la construcción de <strong>modelos computacionales
+                    y su implementación</strong>. Su trabajo exige una constante interacción con la <strong>comunidad científica</strong>, con el fin de desarrollar
+                    nuevos conocimientos y técnicas computacionales.</div>
+            </div><!--//container-->
+
+            <div class="container">
+                <h3 class="section-heading text-center mb-3">Sobre la Semana</h3>
+                <div class="section-intro single-col-max mx-auto mb-4">
+                    La Semana de la Computación es un evento organizado anualmente por la Escuela Profesional de Ciencia de la Computación de la
+                    Universidad Nacional de San Agustín de Arequipa, en la cual se exponen los distintos trabajos y logros
+                    de estudiantes y docentes, además de contar con una serie de ponencias <strong>nacionales e internacionales</strong>, tanto
+                    de la industria como la academia del software. No bastando con las actividades académicas, también se realizan
+                    una serie de Actividades Sociales y Competitivas, como el concurso de programación y los concursos de talentos.</div>
+                <div class="benefits-list text-center mb-3">
+                    <h4 class="text-center mb-4">¿Por qué participar?</h4>
+                    <ul class="list-unstyled text-left d-inline-block">
+                        <li><i class="fas fa-check-circle mr-2"></i>Tendrás la oportunidad para conocer más sobre nosotros como escuela</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Recibirás charlas de los mejores ponentes nacionales e internacionales en Computación</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Descubrirás el estado del arte de las distintas áreas de interés en Computación</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Conocerás nuevas personas y tendrás nuevos contactos de distintos lugares</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Tendrás la oportunidad de participar en los distintos concursos</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Podrás participar de las actividades de confraternización</li>
+                        <li><i class="fas fa-check-circle mr-2"></i>Si eres alumno, podrás presentar tus proyectos y así iniciarte como investigador</li>
+                    </ul>
+                </div><!--//benefits-list-->
+                <div class="event-countdown text-center mb-3">
+                    <h4 class="countdown-intro mb-2 text-center mb-3">¡El Evento ya comenzó!</h4>
+                    <!--<div id="countdown-box" class="countdown-box"></div>-->
+                </div><!--//event-countdown-->
+                <!-- Register -->
+                <!--<div class="about-cta text-center mb-5"><a class="btn btn-secondary btn-lg mb-5 scrollto" href="#venue-section">¡Regístrate Hoy!</a></div>-->
+            </div><!--//container-->
+
+            <div class="media-block theme-bg-primary py-5">
+                <div class="container">
+                    <h4 class="text-white text-center mb-3">Antes de Comenzar</h4>
+                    <div class="section-intro text-center single-col-max mx-auto text-white mb-5">
+                        Antes de comenzar, te proponemos revisar el video de nuestra 1ra Edición de la Semana de la Computación, la cual se dio en el año 2018 de forma presencial.
+                        Debido a la situación actual, esta 3ra Edición de la Semana de la Computación será de forma virtual, pero a pesar de ello, esta misma se hará de la mejor
+                        forma posible gracias a la preparación que venimos realizando previamente.
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-md-12 col-lg-6 offset-lg-3 mb-3">
+                            <div class="embed-responsive embed-responsive-16by9">
+
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/TThtTUnP0z4" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div><!--//embed-responsive -->
+                        </div><!--//col-->
+                    </div><!--//row-->
+                </div><!--//container-->
+            </div><!--//media-block-->
+    </section><!--//about-section-->    
+</template>
+
+<script>
+export default {
+    name: "Aboutus"
+}
+</script>
