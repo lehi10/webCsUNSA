cb42088a376c26a472a46726eb25fbb2e5a73d4a
alrus2797 <alrus2797@gmail.com>
2021-11-04 16:43:58 -0500

    Bases de concursos

--- resources/js/components/AboutusComponent.vue
+++ resources/js/components/AboutusComponent.vue
@@ -8,14 +8,15 @@
         El evento, gratuito y abierto, llega el 2021 a su 4ta edición, esta vez online, con mucho contenido.
         Tendremos más de 20 conferencias sobre los más diversos temas de la computación, con personalidades nacionales e internacionale de la industria como de la academia.
         Las charlas se transmitirán por las plataformas Zoom y 
-        <a href="https://www.facebook.com/csunsa" target="_blank">Facebook</a> de la EPCC UNSA.
-        También tendremos actividades dirigidas a estudiantes de computación, como:
+        <a href="https://www.facebook.com/epcc.unsa" target="_blank">Facebook</a> de la EPCC UNSA.
+        También tendremos actividades dirigidas a estudiantes y egresados de la Escuela Profesional de Ciencia de la Computación, como el:
       </div>
+      
       <div class="benefits-list text-center">
         <ul class="list-unstyled text-left d-inline-block">
-        <li><i class="fas fa-check-circle mr-2"></i> De programación competitiva</li>
-        <li><i class="fas fa-check-circle mr-2"></i> De posters científicos</li>
-        <li><i class="fas fa-check-circle mr-2"></i> De logo de la EPCC-UNSA.</li>
+        <li><i class="fas fa-check-circle mr-2"></i> Concurso de programación competitiva <a href="https://drive.google.com/file/d/1eIZL1Q53tVc19js8aQGENIJxyvpTNHFq/view?usp=sharing" target="_blank">(bases)</a> </li>
+        <li><i class="fas fa-check-circle mr-2"></i>       Concurso de posters científicos <a href="https://drive.google.com/file/d/1q2tpcUjhqJlkg7r6rgeSsKXJQqpi6tGS/view?usp=sharing" target="_blank">(bases)</a></li>
+        <li><i class="fas fa-check-circle mr-2"></i>       Concurso de logo de la EPCC-UNSA <a href="https://drive.google.com/file/d/1SEO-Pt61dY6bRGfJpiPS-w6I6VoiSPB_/view?usp=sharing" target="_blank">(bases)</a></li>
         </ul>
       </div>
       <!--//benefits-list-->
